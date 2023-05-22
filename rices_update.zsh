dbd5fac131db7b72d42e6165bdbb89dffcdfa296
FedericoBustaffa <kiko.bustaffa@gmail.com>
2023-05-22 18:51:37 +0200

    rices update script

---
+++ rices_update.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/zsh
+
+cp -r /home/federico/.config/i3/ /home/federico/rices/
+cp -r /home/federico/.config/polybar/
+cp -r /home/federico/.config/picom/
+cp -r /home/federico/.config/rofi/
+cp -r /home/federico/.config/nvim/
