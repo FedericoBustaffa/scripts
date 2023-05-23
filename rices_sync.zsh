f06be6a0351c26efc1ceab33c5411eadc53d6e17
FedericoBustaffa <kiko.bustaffa@gmail.com>
2023-05-23 17:43:41 +0200

    rices

--- rices_sync.zsh
+++ rices_sync.zsh
@@ -1,7 +1,28 @@
 #!/usr/bin/zsh
 
-cp -r /home/federico/rices/i3/ /home/federico/.config/
-cp -r /home/federico/rices/polybar/ /home/federico/.config/
-cp -r /home/federico/rices/picom/ /home/federico/.config/
-cp -r /home/federico/rices/rofi/ /home/federico/.config/
-cp -r /home/federico/rices/nvim/ /home/federico/.config/
+# terminal
+cp -r /home/federico/rices/alacritty/	/home/federico/.config/
+
+# i3
+cp -r /home/federico/rices/i3/ 			/home/federico/.config/
+cp -r /home/federico/rices/polybar/ 	/home/federico/.config/
+cp -r /home/federico/rices/i3blocks/ 	/home/federico/.config/
+cp -r /home/federico/rices/i3status/ 	/home/federico/.config/
+cp -r /home/federico/rices/picom/ 		/home/federico/.config/
+cp -r /home/federico/rices/rofi/ 		/home/federico/.config/
+
+# neovim
+cp -r /home/federico/rices/nvim/ 		/home/federico/.config/
+cp -r /home/federico/rices/coc/ 		/home/federico/.config/
+
+# hyprland
+cp -r /home/federico/rices/hypr/ 		/home/federico/.config/
+cp -r /home/federico/rices/waybar/ 		/home/federico/.config/
+cp -r /home/federico/rices/swaylock/ 	/home/federico/.config/
+cp -r /home/federico/rices/wofi/ 		/home/federico/.config/
+
+# neofetch
+cp -r /home/federico/rices/neofetch/ 	/home/federico/.config/
+
+# zathura pdf viewer
+cp -r /home/federico/rices/zathura/		/home/federico/.config/
--- rices_update.zsh
+++ rices_update.zsh
@@ -1,7 +1,28 @@
 #!/usr/bin/zsh
 
-cp -r /home/federico/.config/i3/ /home/federico/rices/
-cp -r /home/federico/.config/polybar/ /home/federico/rices/
-cp -r /home/federico/.config/picom/ /home/federico/rices/
-cp -r /home/federico/.config/rofi/ /home/federico/rices/
-cp -r /home/federico/.config/nvim/ /home/federico/rices/
+# terminal
+cp -r /home/federico/.config/alacritty/ /home/federico/rices/
+
+# i3
+cp -r /home/federico/.config/i3/ 		/home/federico/rices/
+cp -r /home/federico/.config/polybar/ 	/home/federico/rices/
+cp -r /home/federico/.config/i3blocks/ 	/home/federico/rices/
+cp -r /home/federico/.config/i3status/ 	/home/federico/rices/
+cp -r /home/federico/.config/picom/ 	/home/federico/rices/
+cp -r /home/federico/.config/rofi/ 		/home/federico/rices/
+
+# neovim
+cp -r /home/federico/.config/nvim/ 		/home/federico/rices/
+cp -r /home/federico/.config/coc/ 		/home/federico/rices/
+
+# hyprland
+cp -r /home/federico/.config/hypr/ 		/home/federico/rices/
+cp -r /home/federico/.config/waybar/ 	/home/federico/rices/
+cp -r /home/federico/.config/swaylock/ 	/home/federico/rices/
+cp -r /home/federico/.config/wofi/ 		/home/federico/rices/
+
+# neofetch
+cp -r /home/federico/.config/neofetch/ 	/home/federico/rices/
+
+# zathura pdf viewer
+cp -r /home/federico/.config/zathura/ 	/home/federico/rices/
