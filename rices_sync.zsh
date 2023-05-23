eb6eb8ff8fef3a9167f0b3c1f7111717e1955ade
FedericoBustaffa <kiko.bustaffa@gmail.com>
2023-05-23 10:16:50 +0200

    sync rices

---
+++ rices_sync.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/zsh
+
+cp -r /home/federico/rices/i3/ /home/federico/.config/
+cp -r /home/federico/rices/polybar/ /home/federico/.config/
+cp -r /home/federico/rices/picom/ /home/federico/.config/
+cp -r /home/federico/rices/rofi/ /home/federico/.config/
+cp -r /home/federico/rices/nvim/ /home/federico/.config/
